2ced3129b4240497a807dad49c5361cf6eadc9e4
Safir Kebieh <safeer-k@hotmail.com>
2025-08-30 23:36:50 +0200

    commit all from my local pc

--- obj/Release/net8.0-windows/QRCodeGenerator.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/QRCodeGenerator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRCodeGenerator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a7e673bbebbb8859525d5909061872bcb985a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRCodeGenerator")]
